d4e293afe3311c2a109eab5c7d48d02920119e4e
dolludaa <dolontaeva00@gmail.com>
2024-01-07 15:24:14 -0500

    Xcode proj created

---
+++ TokenApp/TokenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TokenApp
+//
+//  Created by Людмила Долонтаева on 2024-01-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
